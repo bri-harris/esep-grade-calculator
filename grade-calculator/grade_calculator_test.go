695ce76ade066d947f2dc1f63559a9e6a5266344
Harris <bri.harris@disney.com>
2025-09-30 18:09:44 -0400

    part 3, fix code implementation and F unit test

--- grade-calculator/grade_calculator_test.go
+++ grade-calculator/grade_calculator_test.go
@@ -12,7 +12,6 @@ func TestGetGradeA(t *testing.T) {
 	gradeCalculator.AddGrade("essay on ai ethics", 100, Essay)
 
 	actual_value := gradeCalculator.GetFinalGrade()
-
 	if expected_value != actual_value {
 		t.Errorf("Expected GetGrade to return '%s'; got '%s' instead", expected_value, actual_value)
 	}
@@ -28,7 +27,6 @@ func TestGetGradeB(t *testing.T) {
 	gradeCalculator.AddGrade("essay on ai ethics", 85, Essay)
 
 	actual_value := gradeCalculator.GetFinalGrade()
-
 	if expected_value != actual_value {
 		t.Errorf("Expected GetGrade to return '%s'; got '%s' instead", expected_value, actual_value)
 	}
@@ -39,12 +37,11 @@ func TestGetGradeF(t *testing.T) {
 
 	gradeCalculator := NewGradeCalculator()
 
-	gradeCalculator.AddGrade("open source assignment", 100, Assignment)
-	gradeCalculator.AddGrade("exam 1", 95, Exam)
-	gradeCalculator.AddGrade("essay on ai ethics", 91, Essay)
+	gradeCalculator.AddGrade("open source assignment", 20, Assignment)
+	gradeCalculator.AddGrade("exam 1", 14, Exam)
+	gradeCalculator.AddGrade("essay on ai ethics", 24, Essay)
 
 	actual_value := gradeCalculator.GetFinalGrade()
-
 	if expected_value != actual_value {
 		t.Errorf("Expected GetGrade to return '%s'; got '%s' instead", expected_value, actual_value)
 	}
